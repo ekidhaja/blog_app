b6e53dd65d8e928e2ff5a5d07d525dccc0c74d97
ekidhaja <ekidhaja@gmail.com>
2022-03-30 14:21:31 +0100

    Layout created and navbar added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/global.scss'
+import '../styles/global.scss';
+import { Layout } from '../components';
+import { useState, useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
